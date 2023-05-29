8625972f14ecb17803aa929a0c974a72d6347038
Reid Hansell <reidhansell@gmail.com>
2023-05-29 02:41:27 -0600

    create database tables

--- server.js
+++ server.js
@@ -1,46 +1,27 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
 const path = require('path');
-const mysql = require('mysql');
+const dbManager = require('./databaseManager');
 const config = require('./config.json');
 
-const connection = mysql.createConnection({
-    host: config.dbhostname,
-    user: config.dbusername,
-    password: config.dbpassword,
-    database: config.dbname
-});
+dbManager.createTables();
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to Db', err);
-        process.exit(1); // Exit the process with a "failure" code
-    }
-    console.log('Connection established');
-});
+const port = config.port || 5000;
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (config.production) {
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-// "catchall" handler for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+    // "catchall" handler for any request that doesn't
+    // match one above, send back React's index.html file.
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+}
 
 /* Example route
 app.get('/api', (req, res) => {
     res.send('Hello World from API!');
-});
-*/
-
-/* Example query
-connection.query('SELECT * FROM my_table', (err, rows) => {
-  if(err) throw err;
-
-  console.log('Data received from Db:\n');
-  console.log(rows);
 });
 */
 
