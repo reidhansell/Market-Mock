7d3f96c5279805433f82e761b6351866f84ef020
Reid Hansell <reidhansell@gmail.com>
2023-09-23 03:08:25 -0600

    chart overhaul and additional content

--- client/src/components/Home/Portfolio.tsx
+++ client/src/components/Home/Portfolio.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getUserNetWorthData } from '../../requests/portfolio';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
 import NetWorthData from '../../../../models/NetWorthData';
 import User from '../../../../models/User';
+import LoadingCircle from '../Common/LoadingCircle';
 
 interface Props {
     user: User;
@@ -31,7 +32,7 @@ const Portfolio: React.FC<Props> = ({ user }) => {
     }, [userId]);
 
     const transformToChartData = (data: NetWorthData[]) => {
-        return data.map(d => ({ date: new Date(d.recorded_at).toLocaleDateString(), netWorth: d.net_worth }));
+        return data.map(d => ({ date: new Date(d.recorded_at).toISOString(), netWorth: d.net_worth }));
     };
 
     let chartData: { date: string; netWorth: number }[] = [];
@@ -41,26 +42,57 @@ const Portfolio: React.FC<Props> = ({ user }) => {
     }
 
     return (
-        <div>
-            <LineChart
-                width={300}
-                height={200}
-                data={chartData}
-                margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
-            >
-                <XAxis dataKey="date" />
-                <YAxis
-                    domain={['dataMin', 'dataMax']}
-                    tickFormatter={(tick) => `${(tick / 1000).toFixed(1)}k`}
-                    style={{ fontSize: '12px' }}
-                    width={50}
-                />
-                <Tooltip />
-                <CartesianGrid stroke="#f5f5f5" />
-                <Line type="monotone" dataKey="netWorth" stroke="#ff7300" yAxisId={0} />
-            </LineChart>
-            you started with {user.starting_amount} and now you have {user.current_balance}
-        </div>
+        <>
+            <div style={{ width: '100%', height: '200px' }}>
+                <ResponsiveContainer>
+                    <LineChart
+                        data={chartData}
+                        margin={{ top: 0, right: 40, left: 0, bottom: 0 }}
+                    >
+                        <XAxis
+                            dataKey="date"
+                            tickFormatter={(dateStr) => {
+                                const date = new Date(dateStr);
+                                return `${date.getMonth() + 1}/${date.getDate()}`; // format date to only show day/month
+                            }}
+                            style={{ fontSize: '12px' }}
+                            tick={{ fill: 'white' }}
+                        />
+                        <YAxis
+                            domain={['dataMin', 'dataMax']}
+                            tickFormatter={(tick) => `${(tick / 1000).toFixed(1)}k`}
+                            style={{ fontSize: '12px' }}
+                            width={40}
+                            axisLine={{ stroke: '#f5f5f5' }}
+                            tick={{ fill: 'white' }}
+                        />
+                        <Tooltip />
+                        <CartesianGrid stroke="#f5f5f5" vertical={false} />
+                        <Line type="monotone" dataKey="netWorth" stroke="#3cb043" yAxisId={0} />
+                        {chartData.length > 0 && <ReferenceLine x={chartData[chartData.length - 1].date} stroke="#f5f5f5" />}
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+            <br />
+            <p>
+                Initial Investment:&nbsp;
+                <strong>${user.starting_amount}</strong>
+            </p>
+            <br />
+            <p>
+                Current Wallet Balance:&nbsp;
+                <strong>${user.current_balance}</strong>
+            </p>
+            <br />
+            <p>
+                Current Net Worth:&nbsp;
+                <strong style={{
+                    color: data && data[data.length - 1].net_worth >= user.starting_amount ? '#3cb043' : '#e74c3c'
+                }}>
+                    {data ? `$${data[data.length - 1].net_worth} (+$${data[data.length - 1].net_worth - user.starting_amount} / +%${data[data.length - 1].net_worth / user.starting_amount * 100 - 100})` : <LoadingCircle />}
+                </strong>
+            </p>
+        </>
     );
 };
 
