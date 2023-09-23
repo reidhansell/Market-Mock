f43a5149fa8c06f72efc86def781d78d5b4cfeda
Reid Hansell <reidhansell@gmail.com>
2023-09-23 03:09:27 -0600

    implement watchlist

--- models/WatchList.ts
+++ models/WatchList.ts
@@ -1,5 +1,8 @@
+import TickerIntraday from './TickerIntraday';
+
 export default interface WatchList {
-    watch_list_id: number;
-    user_id: number;
+    user_id?: number; // Does not return to user
     ticker_symbol: string;
+    company_name?: string; // Does not go to server
+    ticker_data?: TickerIntraday; // Does not go to server
 }
