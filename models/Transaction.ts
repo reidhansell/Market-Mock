144f6b474dce87af4a349f984e0dafa31ac381e8
Reid Hansell <reidhansell@gmail.com>
2023-09-26 09:20:16 -0600

    move values to order

--- models/Transaction.ts
+++ models/Transaction.ts
@@ -1,9 +1,6 @@
 export default interface Transaction {
     transaction_id: number;
-    user_id: number;
-    ticker_symbol: string;
-    transaction_type: string;
-    quantity: number;
+    order_id: number;
     price_per_share: number;
     transaction_date: Date;
 }
