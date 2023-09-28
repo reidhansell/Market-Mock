80abf2ec02a5f6f348b593df2b9112f3c2310d1a
Reid Hansell <reidhansell@gmail.com>
2023-09-28 08:21:39 -0600

    add optional version for extending

--- models/Transaction.ts
+++ models/Transaction.ts
@@ -4,3 +4,10 @@ export default interface Transaction {
     price_per_share: number;
     transaction_date: Date;
 }
+
+export interface OptionalTransaction {
+    transaction_id?: number;
+    order_id?: number;
+    price_per_share?: number;
+    transaction_date?: Date;
+}
