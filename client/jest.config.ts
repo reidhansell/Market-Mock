72d6454298ffcaf0bb3a5a87bd9f7c34f1c61309
Reid Hansell <reidhansell@gmail.com>
2023-10-03 08:56:25 -0600

    fix initial test errors

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -2,10 +2,13 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     roots: ['<rootDir>/src/'],
-    testMatch: ['<rootDir>/tests/**/*.test.tsx'],
+    testMatch: ['<rootDir>/src/tests/**/*.test.tsx'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ['./jest.setup.js']
+    setupFilesAfterEnv: ['./jest.setup.ts'],
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "<rootDir>/src/tests/mocks/styleMock.tsx"
+    }
 };
--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,43 +1,39 @@
 import request from 'supertest';
 import app from '../server';
 import { Server } from 'http';
-import { closeDatabaseConnection, closeTransactionPool } from '../database/databaseConnector';
-import CronJobs from '../tools/jobs/CronJobs';
 
 jest.mock('../tools/utils/Logger', () => ({
-    initialize: jest.fn(),
+    initialize: jest.fn(() => null),
 }));
 
 jest.mock('../tools/utils/Router', () => ({
-    initialize: jest.fn(),
+    initialize: jest.fn(() => null),
 }));
 
 jest.mock('../tools/jobs/CronJobs', () => ({
-    scheduleJobs: jest.fn(),
-    stopAll: jest.fn(),
+    scheduleJobs: jest.fn(() => null)
 }));
 
 jest.mock('../database/databaseConnector', () => ({
-    initializeDatabaseConnection: jest.fn(),
+    initializeDatabaseConnection: jest.fn(() => null)
 }));
 
 jest.mock('../database/databaseInitializer', () => {
-    return jest.fn(() => Promise.resolve());
+    return jest.fn(() => Promise.resolve(null));
 });
 
 describe('Server Initialization', () => {
     let server: Server;
 
     beforeAll(() => {
-        server = app.listen(3000);  // or whatever your intended port is
+        server = app.listen(3000);
     });
 
     afterAll((done) => {
-        server.close(done);  // close the server after tests
+        server.close(done);
     });
     it('should start and listen on the correct port', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(404);
     });
 });
-
