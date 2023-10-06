21cb767ac7736d10349de242861a196677d0b19b
Reid Hansell <reidhansell@gmail.com>
2023-10-06 07:55:39 -0600

    add testing id

--- client/src/components/Auth/Login.tsx
+++ client/src/components/Auth/Login.tsx
@@ -50,12 +50,12 @@ const Login: FC<LoginProps> = ({ setAuth }) => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <button type="submit">Login</button>
-                <div className="auth-link">
-                    <small><i>Don't have an account?</i></small>
-                    <button type="button" style={{ width: '100%' }} onClick={() => navigate('/register')}>Register</button>
-                </div>
+                <button type="submit" data-testid="login-page-login-button">Login</button>
             </form>
+            <div className="auth-link">
+                <small><i>Don't have an account?</i></small>
+                <button data-testid="login-page-register-button" type="button" style={{ width: '100%' }} onClick={() => navigate('/register')}>Register</button>
+            </div>
         </div >
     );
 };
