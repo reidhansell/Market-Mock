4266f150169b4f89785517dde4be4e052990f7b2
Reid Hansell <reidhansell@gmail.com>
2023-10-06 07:54:42 -0600

    CRA to Vite

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,14 +1,14 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    roots: ['<rootDir>/src/'],
-    testMatch: ['<rootDir>/src/tests/**/*.test.tsx'],
+    roots: ['<rootDir>'],
+    testMatch: ['**/*.test.tsx'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     setupFilesAfterEnv: ['./jest.setup.ts'],
     moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "<rootDir>/src/tests/mocks/styleMock.tsx"
-    }
+        "\\.(css|less|scss|sass)$": "<rootDir>/src/components/Common/styleMock.tsx"
+    },
 };
