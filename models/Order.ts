eddb5d822c9e76e7b4c6c125cbe96c8d4e867a62
Reid Hansell <reidhansell@gmail.com>
2023-10-08 05:50:00 -0600

    add client side model for order

--- models/Order.ts
+++ models/Order.ts
@@ -1,15 +1,19 @@
-export interface OrderSubmission {
-    order_id: number;
-    user_id: number;
+export interface OrderClientSubmission {
     ticker_symbol: string;
     order_type: string;
     trigger_price: number;
     quantity: number;
 }
 
+export interface OrderSubmission extends OrderClientSubmission {
+    order_id: number;
+    user_id: number;
+}
+
 export default interface Order extends OrderSubmission {
     cancelled: boolean;
     order_date: number;
+    transaction_id?: number;
 }
 
 export interface FulfilledOrder extends Order {
