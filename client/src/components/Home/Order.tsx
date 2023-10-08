881f5895b4d25a22f32901d5f165d6088e408c3e
Reid Hansell <reidhansell@gmail.com>
2023-10-08 04:59:51 -0600

    add tooltip for order details

--- client/src/components/Home/Order.tsx
+++ client/src/components/Home/Order.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { UserContext } from '../Common/UserProvider';
 import { FulfilledOrder } from '../../../../models/Order';
 import DashboardModule from '../Common/DashboardModule';
+import Tooltip from '../Common/Tooltip';
 
 const Order: React.FC = () => {
     const navigate = useNavigate();
@@ -22,15 +23,21 @@ const Order: React.FC = () => {
     const OrderDetails: React.FC<{ order: FulfilledOrder }> = ({ order }) => {
         return (
             <>
-                <h3 style={{ color: order.quantity < 0 ? "red" : "var(--brand)" }} className='owned-stock-header'>
-                    {`${order.ticker_symbol} (${Math.abs(order.quantity)})`}
-                </h3>
+                <h2 className='owned-stock-header'>{`${order.ticker_symbol} (${Math.abs(order.quantity)})`} <Tooltip text={["Status: Open, Fulfilled, or Cancelled.",
+                    "",
+                    "Trigger Price: What the order will attempted to be fulfilled for.",
+                    "",
+                    "Fulfilled price: What the order was actually fulfilled for",
+                    "",
+                    "Date Placed: When the order was placed",
+                    "",
+                    "Date Fulfilled: When the order was fulfilled"]} /></h2>
                 <p style={{ color: order.quantity < 0 ? "red" : "var(--brand)" }}>{order.quantity < 0 ? "SELL" : "BUY"}</p>
                 <p>Status: {order.cancelled ? "Cancelled" : order.transaction_id ? "Fulfilled" : "Open"}</p>
                 <p>Trigger Price: {order.trigger_price}</p>
                 {order.transaction_id && <p>Fulfilled price: {order.price_per_share}</p>}
-                <p>Date Placed: {date.toLocaleDateString()}</p>
-                {order.transaction_id && <p>Date Fulfilled: {date.toLocaleDateString()}</p>}
+                <p>Date Placed: {new Date(date).toLocaleDateString()}</p>
+                {order.transaction_id && <p>Date Fulfilled: {new Date(date).toLocaleDateString()}</p>}
             </>
         );
     };
