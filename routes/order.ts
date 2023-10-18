5bca7cd0defea4570fe9786f45e5f56a88dfd556
Reid Hansell <reidhansell@gmail.com>
2023-10-18 03:32:35 -0600

    extract quest logic for DRY/SRP

--- routes/order.ts
+++ routes/order.ts
@@ -12,6 +12,21 @@ interface AuthenticatedRequest extends Request {
     }
 }
 
+async function handleOrderQuests(user_id: number, order: Order) {
+    const quests = await getQuests(user_id);
+    const buyOrderQuest = quests.find(quest => quest.name === 'Place a buy order');
+    const sellOrderQuest = quests.find(quest => quest.name === 'Place a sell order');
+    const limitOrderQuest = quests.find(quest => quest.name === 'Place a limit order');
+    const stopOrderQuest = quests.find(quest => quest.name === 'Place a stop order');
+    const marketOrderQuest = quests.find(quest => quest.name === 'Place a market order');
+
+    buyOrderQuest && buyOrderQuest.completion_date === null && order.quantity > 0 ? await updateQuest(user_id, buyOrderQuest.quest_id) : null;
+    sellOrderQuest && sellOrderQuest.completion_date === null && order.quantity < 0 ? await updateQuest(user_id, sellOrderQuest.quest_id) : null;
+    limitOrderQuest && limitOrderQuest.completion_date === null && order.order_type === 'LIMIT' ? await updateQuest(user_id, limitOrderQuest.quest_id) : null;
+    stopOrderQuest && stopOrderQuest.completion_date === null && order.order_type === 'STOP' ? await updateQuest(user_id, stopOrderQuest.quest_id) : null;
+    marketOrderQuest && marketOrderQuest.completion_date === null && order.order_type === 'MARKET' ? await updateQuest(user_id, marketOrderQuest.quest_id) : null;
+}
+
 const router = Router();
 
 router.get('/', authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
@@ -47,7 +62,6 @@ router.post('/', authenticateToken, async (req: Request, res: Response, next: Ne
             throw new ExpectedError("Invalid quantity. Quantity must be a non-zero number.", 400, "Order creation failed with invalid quantity");
         }
 
-        console.log(`User ${user_id} is attempting to create an order with the following parameters: ${JSON.stringify(req.body)}`)
         const order = await insertOrder({
             user_id,
             ticker_symbol,
@@ -55,24 +69,11 @@ router.post('/', authenticateToken, async (req: Request, res: Response, next: Ne
             trigger_price,
             quantity,
         } as Order);
-        console.log(`User ${user_id} successfully created an order with the following return: ${JSON.stringify(order)}`)
 
-        const quests = await getQuests(user_id);
-        const buyOrderQuest = quests.find(quest => quest.name === 'Place a buy order');
-        const sellOrderQuest = quests.find(quest => quest.name === 'Place a sell order');
-        const limitOrderQuest = quests.find(quest => quest.name === 'Place a limit order');
-        const stopOrderQuest = quests.find(quest => quest.name === 'Place a stop order');
-        const marketOrderQuest = quests.find(quest => quest.name === 'Place a market order');
+        await handleOrderQuests(user_id, order);
 
-        buyOrderQuest && buyOrderQuest.completion_date === null && order.quantity > 0 ? await updateQuest(user_id, buyOrderQuest.quest_id) : null;
-        sellOrderQuest && sellOrderQuest.completion_date === null && order.quantity < 0 ? await updateQuest(user_id, sellOrderQuest.quest_id) : null;
-        limitOrderQuest && limitOrderQuest.completion_date === null && order.order_type === 'LIMIT' ? await updateQuest(user_id, limitOrderQuest.quest_id) : null;
-        stopOrderQuest && stopOrderQuest.completion_date === null && order.order_type === 'STOP' ? await updateQuest(user_id, stopOrderQuest.quest_id) : null;
-        marketOrderQuest && marketOrderQuest.completion_date === null && order.order_type === 'MARKET' ? await updateQuest(user_id, marketOrderQuest.quest_id) : null;
-
-        console.log(`User ${user_id} is attempting to process an order with the following parameters: ${JSON.stringify(order)}`)
         const resultingOrder = await processOrder(order);
-        console.log(`User ${user_id} successfully processed an order with the following return: ${JSON.stringify(resultingOrder)}`)
+
         if (resultingOrder === null) {
             return res.json(order);
         } else {
