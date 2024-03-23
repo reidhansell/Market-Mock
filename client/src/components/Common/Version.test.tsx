cae2f05764d7d6049970fe797f7983038c74abf6
Gravity <gravity@swgr.org>
2024-03-23 01:29:06 -0600

    remove jest, prepare for switch to cloudscape

--- client/src/components/Common/Version.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Version from './Version';
-
-test('renders version component', () => {
-    render(<Version version="1.0.0" />);
-});
